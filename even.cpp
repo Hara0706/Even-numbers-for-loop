7ce6b0a70fc751d170a8fa83716c21cfd88df564
Harapriya Mohapatra <73417004+Hara0706@users.noreply.github.com>
2021-07-08 10:39:20 +0530

    Create even.cpp

---
+++ even.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+
+
+
+    int a = 2;
+
+    while(a<=100)
+
+    {
+
+        cout<<a<<endl;
+
+        a = a+2;
+
+    }
+
+   return 0;
+
+}
